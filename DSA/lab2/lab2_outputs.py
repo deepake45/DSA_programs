fa65994f74a8ca42633250d7823a7376865eb5e2
deepake45 <deepakelango.45@gmail.com>
2026-08-06 14:01:24 +0530

    Add files via upload

---
+++ DSA/lab2/lab2_outputs.py
@@ -0,0 +1,180 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========================================== RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q1.py ==========================================
+Enter no of elements : 5
+enter the elements of the array : 10
+enter the elements of the array : 20
+enter the elements of the array : 30
+enter the elements of the array : 40
+enter the elements of the array : 50
+Enter the element to search : 40
+Element found at index 3
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Traceback (most recent call last):
+  File "C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py", line 18, in <module>
+    arr.append(a)
+NameError: name 'arr' is not defined
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements of the array : 50
+Enter the elements to search : 50
+Element found at the index :  <function binary_search at 0x000001D513D9FC40>
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 304
+Enter the elements of the array : 305
+Enter the elements of the array : 400
+Enter the elements to search : 400
+Traceback (most recent call last):
+  File "C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py", line 21, in <module>
+    result = binary_search(arr, key)
+  File "C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py", line 8, in binary_search
+    if arr[mid] == ket:
+NameError: name 'ket' is not defined. Did you mean: 'key'?
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements of the array : 50
+Enter the elements of the array : 40
+Enter the elements of the array : 30
+Enter the elements of the array : 20
+Enter the elements of the array : 10
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements to search : 50
+Element is not found
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 
+Traceback (most recent call last):
+  File "C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py", line 15, in <module>
+    n  = int(input("Enter no of elements : "))
+ValueError: invalid literal for int() with base 10: ''
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 
+Traceback (most recent call last):
+  File "C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py", line 15, in <module>
+    n  = int(input("Enter no of elements : "))
+ValueError: invalid literal for int() with base 10: ''
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements of the array : 50
+Enter the elements to search : 50
+Element is not found
+arr
+[10, 20, 30, 40, 50]
+high
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    high
+NameError: name 'high' is not defined
+low
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    low
+NameError: name 'low' is not defined. Did you mean: 'pow'?
+>>> key
+50
+>>> result
+-1
+>>> 
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 4
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements to search : 30
+Element found at the index :  2
+>>> 
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements of the array : 50
+Enter the elements to search : 50
+Element is not found
+>>> 
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements of the array : 5
+Enter the elements to search : 5
+Element is not found
+>>> 
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 2
+Enter the elements of the array : 
+Traceback (most recent call last):
+  File "C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py", line 18, in <module>
+    a = int(input("Enter the elements of the array : "))
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(a).py =========================================
+Enter no of elements : 5
+Enter the elements of the array : 10
+Enter the elements of the array : 20
+Enter the elements of the array : 30
+Enter the elements of the array : 40
+Enter the elements of the array : 50
+Enter the elements to search : 50
+Element found at the index :  4
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(b).py =========================================
+Enter the no of elements of the array : 4
+Enter the elements of the array : 10
+Enter the elements of the array : 4
+Enter the elements of the array : 5
+Enter the elements of the array : 3
+Enter the element to search : 2
+The input list is not sorted
+Sorting the element
+Sorted List :  [3, 4, 5, 10]
+Element not found
+arr
+[3, 4, 5, 10]
+result
+-1
+key
+2
+
+========================================= RESTART: C:/Users/CSE-Lab1/AppData/Local/Programs/Python/Python312/DSA/lab2/q2(b).py =========================================
+Enter the no of elements of the array : 5
+Enter the elements of the array : 860931
+Enter the elements of the array : 51739
+Enter the elements of the array : 5174
+Enter the elements of the array : 568
+Enter the elements of the array : 748510
+Enter the element to search : 568
+The input list is not sorted
+Sorting the element
+Sorted List :  [568, 5174, 51739, 748510, 860931]
+Element found at the index :  0
